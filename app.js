8e5a9da49dc3c4452925c4b83b677b3098a9385a
samade747 <samad.e747@gmail.com>
2023-10-30 23:11:15 +0500

    448561212

---
+++ app.js
@@ -0,0 +1,20 @@
+// Destructing in javascript
+// array & object par chalega 
+// const abc1 = ['abc', 'def', 'lkm', 'pqrs', 'xyz']
+
+// const [a,b,c,d,e] = abc 
+
+// console.log(c)
+
+
+const obj = {
+    abc : 'ABCD',
+    ddd : 'zzjjh',
+    yyzy: 'addasda',
+    iloi: 'dassadas'
+}
+
+
+const {abc, ddd, yyzy, iloi} = obj
+
+console.log(abc, ddd, yyzy, iloi)
